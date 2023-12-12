1b321966dbff8a7e75845624d496755ac47127d0
ScenerYOne <krittaporn485@gmail.com>
2023-12-12 20:35:54 +0700

    pp

--- Testuser.java
+++ Testuser.java
@@ -1,4 +1,12 @@
-impor
+import java.util.ArrayList;
 public class Testuser {
-    
-}
+    public static void main(String[] args) {
+
+    ArrayList<Student> member = new ArrayList<>();
+    member.add(new Student(65100021 , "AAA", "test11"));
+    member.add(new Student(65100022 , "BBB", "test12"));
+    member.add(new Student(65100023 , "CCC", "test13"));
+                  
+
+    }
+    }
