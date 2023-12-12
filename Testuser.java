08bc0ca6b99f0d87a2a925baef6f430b19d2a646
ScenerYOne <krittaporn485@gmail.com>
2023-12-12 20:55:48 +0700

    pp

--- Testuser.java
+++ Testuser.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
+import java.util.Date;
+
 public class Testuser {
     public static void main(String[] args) {
+        ArrayList<Student> members = new ArrayList<Student>();
 
-    ArrayList<Student> member = new ArrayList<>();
-    member.add(new Student(65100021 , "AAA", "test11"));
-    member.add(new Student(65100022 , "BBB", "test12"));
-    member.add(new Student(65100023 , "CCC", "test13"));
-                  
+        Date expirationDate1 = new Date(); 
+        Date applicationDate1 = new Date(); 
+        members.add(new Student("65100021", "AAA", expirationDate1, applicationDate1));
 
+        Date expirationDate2 = new Date(); 
+        Date applicationDate2 = new Date();
+        members.add(new Student("65100022", "BBB", expirationDate2, applicationDate2));
+
+        Date expirationDate3 = new Date(); 
+        Date applicationDate3 = new Date(); 
+        members.add(new Student("65100023", "CCC", expirationDate3, applicationDate3));
+
+       
+        System.out.println("Input details:");
+        for (Student student : members) {
+            System.out.println("Student ID: " + student.getStudentID() + " Name: " + student.getStudentName());    
+            System.out.println("Expiration Date: " + student.getExpirationDate() +", Application Date: " + student.getApplicationDate());
+        }
     }
-    }
+}
